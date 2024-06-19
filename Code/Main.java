1827593971e16706c0a3fa18a617369029d35a30
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2024-06-19 19:28:07 +0700

    Update Main.java

--- Code/Main.java
+++ Code/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
 
-public class Main{
+final public class Main{
 
     // Stores all Customers
     static ArrayList<Customer> customerList = new ArrayList<>();
