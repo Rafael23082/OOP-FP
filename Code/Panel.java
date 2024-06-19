d3e6149692eb3b95616e96279e1c8283e80ecffa
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2024-06-19 19:31:46 +0700

    Update Panel.java

--- Code/Panel.java
+++ Code/Panel.java
@@ -3,7 +3,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 // Panel class to make the initializing of panels be more efficient
-public class Panel extends JPanel {
+final public class Panel extends JPanel {
     public Panel(int x, int y, int length, int height){
 
         // Create border
